c913deb00740955bfae94ebc9be378f7da507e9d
Nwon2464 <dnjsskarb7@gmail.com>
2021-01-17 05:18:22 +0900

    card tilt finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import { colorData } from "./colorData";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {colorData.map((e, i) => {
+        return (
+          <div className="card">
+            <div style={{ background: `${e.color}` }} className="circle">
+              <h2>0{i + 1}</h2>
+            </div>
+            <div className="content">
+              <p style={{ padding: "1rem" }}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
+                beatae ipsam, excepturi ipsum dolores nostrum aut libero
+                molestias magni sed suscipit voluptatem consectetur repellat,
+                ducimus accusantium. Perspiciatis optio molestias dicta.
+              </p>
+              <a href="#" style={{ background: `${e.color}` }}>Read more</a>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/colorData.js
@@ -0,0 +1,23 @@
+export const colorData = [
+  {
+    color: "#F9AE31",
+  },
+  {
+    color: "#3B5899",
+  },
+  {
+    color: "#333333",
+  },
+  {
+    color: "#CB4024",
+  },
+  {
+    color: "#d9317a",
+  },
+  {
+    color: "#1EB954",
+  },
+  {
+    color: "#5F3BAD",
+  },
+];
